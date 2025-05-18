26e21a219062fd9f71038cfa5eca79a5f309bfab
Emediong Eshiet <eshietemedionguyobong@gmail.com>
2025-05-18 23:59:15 +0100

    Add weather forecasting and the implementation including style

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+app.get("/premium", (req, res) => {
+    res.render("premium");
+})
+
 app.post("/", async (req, res) => {
     try {
         const response = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
@@ -36,7 +40,33 @@ app.post("/", async (req, res) => {
             errorMessage = error.response.data.message || "An error occurred.";
         }
         res.render("index", {
-            errorMessage: errorMessage
+            errorMessage
+        });
+    }
+    
+});
+
+app.post("/premium", async (req, res) => {
+    try {
+        const response = await axios.get("https://api.openweathermap.org/data/2.5/forecast", {
+            params: {
+                appid: apiKey,
+                q: req.body.city,
+                units: "metric"
+            }
+        });
+        const data = response.data;
+        res.render("premium", {
+            weatherInfo: data,
+            // weatherImage:`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        });
+    } catch (error) {
+        let errorMessage = "An error occurred.";
+        if (error.response && error.response.data) {
+            errorMessage = error.response.data.message || "An error occurred.";
+        }
+        res.render("premium", {
+            errorMessage
         });
     }
     
